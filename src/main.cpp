e4983f09f7f45f5e7fd8ce916bd50b31dd58ebbf
AHAOAHA <ahaoaha_@outlook.com>
2020-01-16 00:11:09 +0800

    add third_party

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,22 @@
 *mail: ahaoaha_@outlook.com
 *Created Time: 2019年02月19日 星期二 16时48分45秒
  ************************************************************************/
+#include <glog/logging.h>
+
 #include "HttpServer.hpp"
 
 
-int main(int argc, char* argv[])
-{
-  if(argc != 2)
-  {
-    cout << "./server [port]" << endl;
-    return -1;
-  }
-  HttpServer serv;
-  serv.Start(atoi(argv[1]), handler);
-  return 0;
+int main(int argc, char* argv[]) {
+	if(argc != 2) {
+		cout << "./server [port]" << endl;
+		return -1;
+	}
+	FLAGS_log_dir = "../log";
+	google::InitGoogleLogging(argv[0]);
+
+	HttpServer serv;
+	serv.Start(atoi(argv[1]), handler);
+
+	google::ShutdownGoogleLogging();
+	return 0;
 }
